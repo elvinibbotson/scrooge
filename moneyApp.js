a98e8c08d64525cfef48104c3408537d4ea1dcd3
elvinibbotson <elvinibbotson@outlook.com>
2021-06-03 12:13:26 +0100

    scrooge name and icon

--- moneyApp.js
+++ moneyApp.js
@@ -684,7 +684,7 @@ if(navigator.serviceWorker.controller) {
 	console.log('Active service worker found, no need to register')
 }
 else { // Register the ServiceWorker
-	navigator.serviceWorker.register('moneySW.js', {scope: '/Money/'}).then(function(reg) {
+	navigator.serviceWorker.register('moneySW.js', {scope: '/scrooge/'}).then(function(reg) {
 	    console.log('Service worker has been registered for scope:'+ reg.scope);
 	});
 }
